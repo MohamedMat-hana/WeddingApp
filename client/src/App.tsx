fcf3e0f505c73e5f228f125e7ba1c2bd849c8b57
MohamedMat-hana <mathanamohamed9@gmail.com>
2025-07-12 08:55:06 +0300

    Remove unused Link import from App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Countdown from "react-countdown";
 import "./index.css";
 import SubmissionsPage from "./SubmissionsPage";
@@ -92,7 +92,6 @@ function App() {
       width="50%"
       height="300"
       style={{ border: 0 }}
-      allowFullScreen=""
       loading="lazy"
       referrerPolicy="no-referrer-when-downgrade"
     ></iframe>
