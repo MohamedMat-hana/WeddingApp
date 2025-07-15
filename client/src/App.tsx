002d3cf5bd4aea8a34c3098c324c95f218d32f5f
MohamedMat-hana <mathanamohamed9@gmail.com>
2025-07-15 18:30:34 +0300

    Remove unused Link import from App componento

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Countdown from "react-countdown";
 import "./index.css";
 import SubmissionsPage from "./SubmissionsPage";
